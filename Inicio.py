0cf50e6d5ef2fbbe00fac1102ed0c4d8736a052a
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-01-23 19:41:28 -0300

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+print('SEJA BEM VINDO AO BANK NEW HORIZON !!')
+nome = str(input('Por favor informe o seu nome: '))
+
+resp = int(input('Sr {} o senhor(a) possui cadastro no banco new horizon ? [0]não [1]sim: '.format(nome)))
